d48c119199ccf99da11d28d987be11fe28e42b84
David Hay <david.j.hay@gmail.com>
2021-07-27 11:03:05 +0100

    post lesson 188 - coonfiguring custom hooks

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -3,16 +3,22 @@ import { useState, useEffect } from "react";
 //custom hooks must start with 'use'
 //don't put presentation in here.
 
-const useCounter = (change) => {
+const useCounter = (forwards = true) => {
+  //this state is separate for each use of this custom hook
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => change(prevCounter));
+      setCounter((prevCounter) =>
+        forwards ? prevCounter + 1 : prevCounter - 1
+      );
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
+
+  //you can return whatever you want from custom hook
+  return counter; //gives users access to this counter's state
 };
 
 export default useCounter;
